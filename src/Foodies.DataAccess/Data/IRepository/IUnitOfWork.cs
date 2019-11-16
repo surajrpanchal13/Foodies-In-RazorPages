5f1942c880fbe307a01c3ecf99a047db8f27e4ae
Suraj R Panchal <suraj.school@outlook.com>
2019-11-16 16:38:07 +0530

    Added Category Model & Repository

---
+++ src/Foodies.DataAccess/Data/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foodies.DataAccess.Data.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        void Save();
+    }
+}
---
+++ src/Foodies.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Foodies.DataAccess.Data.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Foodies.DataAccess.Data.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
