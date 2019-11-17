36422f2d62ddd1ee96df907fe636f160a724d836
Suraj <48174433+surajrpanchal13@users.noreply.github.com>
2019-11-17 18:08:44 +0530

    Added Category Page
    Added Category Page

---
+++ src/Foodies/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Foodies.DataAccess.Data.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Taste.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var categpryList = _unitOfWork.Category.GetAll().ToList();
+            return Json(new { data = categpryList });
+        }
+
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.Category.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Category.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete successful" });
+        }
+
+    }
+}
